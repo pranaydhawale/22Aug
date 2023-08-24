0c82d74850a964ab8858a5e47735b0868afecea8
pranaydhawale <142602746+pranaydhawale@users.noreply.github.com>
2023-08-24 23:49:51 +0530

    Commit By Git

--- GitHub/src/GitHub/DemoGitHub.java
+++ GitHub/src/GitHub/DemoGitHub.java
@@ -11,7 +11,11 @@ public class DemoGitHub {
 	{
 		System.out.println("M2");
 	}
-	
+
+	public void m3()
+	{
+		System.out.println("M3");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub");
