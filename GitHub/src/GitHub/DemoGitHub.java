8f43255180b3192841a6a889156a97ab2fe723e8
Pranay <jointeampranay@gmail.com>
2023-08-24 23:43:33 +0530

    By Pranay

--- GitHub/src/GitHub/DemoGitHub.java
+++ GitHub/src/GitHub/DemoGitHub.java
@@ -2,6 +2,12 @@ package GitHub;
 
 public class DemoGitHub {
 
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub");
